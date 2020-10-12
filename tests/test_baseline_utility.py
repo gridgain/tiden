279d40808a76bdce8e1ebd5c7916d967f0b5a73a
Ivan Artiukhov <36443126+iartiukhov@users.noreply.github.com>
2020-10-12 19:05:12 +0300

    add tests into tests/test_baseline_utility.py
    Co-authored-by: MaxLosevskoy <maxlosevskoy@ya.ru>

--- tests/test_baseline_utility.py
+++ tests/test_baseline_utility.py
@@ -21,3 +21,40 @@ def test_simple_negative():
         buff = "hola!\nhello!\nhi!"
         lines_to_search = ["ni hao!"]
         util.check_content_all_required(buff, lines_to_search)
+
+
+def test_partial_match():
+    util = BaseUtility(MockIgnite())
+    buff = "hola!\nhello!\nhi!"
+    lines_to_search = ["hell"]
+    assert util.check_content_all_required(buff, lines_to_search)
+
+
+def test_multiple_lines():
+    util = BaseUtility(MockIgnite())
+    buff = "hola!\nhello!\nhi!"
+    lines_to_search = ["hi!", "hola!"]
+    assert util.check_content_all_required(buff, lines_to_search)
+
+
+def test_repeated_line():
+    util = BaseUtility(MockIgnite())
+    buff = "hola!\nhello!\nhi!"
+    lines_to_search = ["hi!", "hi!"]
+    assert util.check_content_all_required(buff, lines_to_search)
+
+
+def test_multiple_lines_negative():
+    with pytest.raises(TidenException):
+        util = BaseUtility(MockIgnite())
+        buff = "hola!\nhello!\nhi!"
+        lines_to_search = ["hola!", "ni hao!"]
+        util.check_content_all_required(buff, lines_to_search)
+
+
+def test_spaces_negative():
+    with pytest.raises(TidenException):
+        util = BaseUtility(MockIgnite())
+        buff = "hola!\nhello!\nhi!"
+        lines_to_search = [" hi!\n"]
+        util.check_content_all_required(buff, lines_to_search)
