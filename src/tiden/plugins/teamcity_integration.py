dccf56f785016b51e06f52778d75c12031e9ed9a
spilshchikov <pilshchikov.s.n@gmail.com>
2021-03-15 20:18:03 +0300

    format text

--- src/tiden/plugins/teamcity_integration.py
+++ src/tiden/plugins/teamcity_integration.py
@@ -24,9 +24,27 @@ TIDEN_PLUGIN_VERSION = '1.0.0'
 
 class TeamcityIntegration(TidenPlugin):
 
+    def _format_stacktrace(self, text):
+        """
+        Format stacktrace for TC
+        Escape characters need to be changed to |
+
+        https://www.jetbrains.com/help/teamcity/build-script-interaction-with-teamcity.html#BuildScriptInteractionwithTeamCity-Escapedvalues
+        """
+        for symbol in "|'[]":
+            text = text.replace(symbol, f'|{symbol}')
+        if '\n' in text:
+            text = text.replace('\n', '|n')
+        if '\r' in text:
+            text = text.replace('\r', '|r')
+        if '\t' in text:
+            text = text.replace('\t', '|t')
+        return text
+
     def before_test_method(self, *args, **kwargs):
         test_name = kwargs.get('test_name')
         time_stamp = datetime.now().isoformat()[:-3]
+        test_name = self._format_stacktrace(test_name)
 
         log_print(f"##teamcity[testStarted timestamp='{time_stamp}' name='{test_name}']")
 
@@ -36,8 +54,9 @@ class TeamcityIntegration(TidenPlugin):
         exception = str(kwargs.get('exception'))
         status = kwargs.get('test_status')
         time_stamp = datetime.now().isoformat()[:-3]
+        test_name = self._format_stacktrace(test_name)
 
         if status == 'pass':
             log_print(f"##teamcity[testFinished timestamp='{time_stamp}' name='{test_name}']")
         else:
-            log_print(f"##teamcity[testFailed timestamp='{time_stamp}' name='{test_name}' message='{exception}' details='{exception_details}']")
+            log_print(f"##teamcity[testFailed timestamp='{time_stamp}' name='{test_name}' message='{exception}' details='{self._format_stacktrace(exception_details)}']")
